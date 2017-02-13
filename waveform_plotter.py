9f05f5f18839adf5d71e917ddb3f441150a4b03f
David Zhou <dz54@duke.edu>
2017-02-13 15:59:22 -0500

    now indicates selected PVC in a nice way

--- waveform_plotter.py
+++ waveform_plotter.py
@@ -16,7 +16,10 @@ class LinkedView(plugins.PluginBase):
     LinkedViewPlugin.prototype = Object.create(mpld3.Plugin.prototype);
     LinkedViewPlugin.prototype.constructor = LinkedViewPlugin;
     LinkedViewPlugin.prototype.requiredProps = ["idpts", "idline", "data"];
-    LinkedViewPlugin.prototype.defaultProps = {}
+    LinkedViewPlugin.prototype.defaultProps ={alpha_bg:0.3,
+                                              alpha_fg:1.0,
+                                              small_size:1,
+                                              large_size:3}
     function LinkedViewPlugin(fig, props){
         mpld3.Plugin.call(this, fig, props);
     };
@@ -25,12 +28,38 @@ class LinkedView(plugins.PluginBase):
       var pts = mpld3.get_element(this.props.idpts);
       var line = mpld3.get_element(this.props.idline);
       var data = this.props.data;
+      alpha_fg = this.props.alpha_fg;
+      alpha_bg = this.props.alpha_bg;
+      small_size = this.props.small_size;
+      large_size = this.props.large_size;
+      var sel = null;
+
+      if (data.length == 0) {
+        this.fig.canvas.append("text")
+            .text("No PVCs detected")
+            .style("font-size", 40)
+            .style("opacity", 0.3)
+            .style("text-anchor", "middle")
+            .attr("x", this.fig.width / 2)
+            .attr("y", this.fig.height * 3 / 4)
+      }
 
       function mouseover(d, i){
         line.data = data[i];
         line.elements().transition()
             .attr("d", line.datafunc(line.data))
             .style("stroke", this.style.fill);
+        if (sel != null) {
+          d3.select(sel).transition().duration(250)
+                            .style("stroke-width", small_size)
+                            .style("stroke-opacity", alpha_bg)
+                            .style("fill-opacity", alpha_bg);
+        }
+        d3.select(this).transition().duration(250)
+                            .style("stroke-width", large_size)
+                            .style("stroke-opacity", alpha_fg)
+                            .style("fill-opacity", alpha_fg);
+        sel = this;
       }
       pts.elements().on("mouseover", mouseover);
     };
@@ -45,7 +74,11 @@ class LinkedView(plugins.PluginBase):
         self.dict_ = {"type": "linkedview",
                       "idpts": utils.get_id(points, suffix),
                       "idline": utils.get_id(line),
-                      "data": linedata}
+                      "data": linedata,
+                      "alpha_bg": 0.3,
+                      "alpha_fg": 1.0,
+                      "small_size": 1,
+                      "large_size": 3}
 
 
 def render_interactive_plot(lvm, pvcs, window=3):
@@ -81,15 +114,23 @@ def render_interactive_plot(lvm, pvcs, window=3):
     ax[1].set_xlabel('Timeline (' + units + ')')
     ax[1].set_ylabel('PVC %')
     ax[0].set_title("PVC Analyzer")
+    ax[0].set_xlabel('Time (seconds)')
+    ax[0].set_ylabel('ECG signal (mV)')
 
-    pvc_indices = pvcs[:, 0]
-    pvc_times = np.take(time, pvc_indices) / divisor
-    pvc_strengths = pvcs[:, 1]
-    ax[1].vlines(pvc_times, np.zeros(len(pvc_times)), pvc_strengths)
+    try:
+        pvc_indices = pvcs[:, 0]
+        pvc_times = np.take(time, pvc_indices) / divisor
+        pvc_strengths = pvcs[:, 1]
+    except IndexError:
+        # No PVCs detected
+        pvc_indices = []
+        pvc_times = []
+        pvc_strengths = []
+    ax[1].vlines(pvc_times, np.zeros(len(pvc_times)), pvc_strengths, alpha=0.3)
     points = ax[1].scatter(pvc_times,
                            pvc_strengths,
                            c=pvc_strengths,
-                           s=250, alpha=0.7)
+                           s=250, alpha=0.3)
 
     # create the line and data objects
     x = np.take(time, range(0, window_range))
@@ -101,10 +142,15 @@ def render_interactive_plot(lvm, pvcs, window=3):
                       if index + window_range <= len(ecg)
                       else range(len(ecg) - window_range, len(ecg)))]
              for index in pvc_indices])
-    lines = ax[0].plot(x, 0 * x, '-w', lw=3, alpha=0.5)
+    lines = ax[0].plot(x, 0 * x, '-w', lw=3, alpha=0.7)
     ax[0].set_ylim(0, 2)
 
     # transpose line data and add plugin
-    linedata = waveform_data.transpose(0, 2, 1).tolist()
+    try:
+        linedata = waveform_data.transpose(0, 2, 1).tolist()
+    except ValueError:
+        # No PVCs detected
+        linedata = []
+        pass
     plugins.connect(fig, LinkedView(points, lines[0], linedata))
     mpld3.show()

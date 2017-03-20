c4f89e876e3c699b05729cdce4d0ba35be3a7ca6
David Zhou <dz54@duke.edu>
2017-03-20 15:03:25 -0400

    removed main check to allow bokeh serve to work

--- holter_monitor.py
+++ holter_monitor.py
@@ -3,34 +3,35 @@ import argument_parser as ap
 import input_reader as ir
 import waveform_plotter as wp
 
-if __name__ == "__main__":
-    args = ap.parse_arguments()
+args = ap.parse_arguments()
 
-    logging.basicConfig(
-        filename="holter_monitor_log.txt",
-        format='%(asctime)s %(levelname)s %(message)s',
-        datefmt='%m/%d/%Y %I:%M:%S %p',
-        level=args.log)
+logging.basicConfig(
+    filename="holter_monitor_log.txt",
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=args.log)
 
-    log = logging.getLogger("hm_logger")
+log = logging.getLogger("hm_logger")
 
-    if args.ui:
-        pass
-    elif args.convert != "":
-        data = ir.read_data(args.data, args.path)
-        ir.save_binary(data, args.data, args.convert, args.path)
-    else:
-        data = ir.read_data(args.data, args.path)
-        # import matplotlib.pyplot as plt
-        # plt.plot(data)
-        # plt.show()
-        import numpy as np
-        pvcs = np.array(
-            [[13495, 55],
-             [19406, 42],
-             [25305, 73],
-             [143498, 64],
-             [149402, 36],
-             [155301, 81]])
-        # wp.render_pvc_plot(data, pvcs)
-        wp.render_full_plot(data, pvcs)
+log.debug(args)
+
+if args.ui:
+    pass
+elif args.convert != "":
+    data = ir.read_data(args.data, args.path)
+    ir.save_binary(data, args.data, args.convert, args.path)
+else:
+    data = ir.read_data(args.data, args.path)
+    # import matplotlib.pyplot as plt
+    # plt.plot(data)
+    # plt.show()
+    import numpy as np
+    pvcs = np.array(
+        [[13495, 55],
+         [19406, 42],
+         [25305, 73],
+         [143498, 64],
+         [149402, 36],
+         [155301, 81]])
+    # wp.render_pvc_plot(data, pvcs)
+    wp.render_full_plot(data, pvcs)

954afedde484a4a9268a53eb489b7f28a92517ee
David Zhou <dz54@duke.edu>
2017-04-18 14:35:18 -0400

    fixed master

--- filter_functions.py
+++ filter_functions.py
@@ -14,19 +14,19 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
     y = lfilter(b, a, data)
     return y
 
-cutoff=15
-fs=1000
-
-b, a = butter_lowpass(cutoff=15, fs=1000, order=5)
-
-# Plot the frequency response.
-w, h = freqz(b, a, worN=8000)
-plt.subplot(2, 1, 1)
-plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
-plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
-plt.axvline(cutoff, color='k')
-plt.xlim(0, 0.5*fs)
-plt.title("Lowpass Filter Frequency Response")
-plt.xlabel('Frequency [Hz]')
-plt.grid()
-plt.show()
+# cutoff=15
+# fs=1000
+#
+# b, a = butter_lowpass(cutoff=15, fs=1000, order=5)
+#
+# # Plot the frequency response.
+# w, h = freqz(b, a, worN=8000)
+# plt.subplot(2, 1, 1)
+# plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
+# plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
+# plt.axvline(cutoff, color='k')
+# plt.xlim(0, 0.5*fs)
+# plt.title("Lowpass Filter Frequency Response")
+# plt.xlabel('Frequency [Hz]')
+# plt.grid()
+# plt.show()
